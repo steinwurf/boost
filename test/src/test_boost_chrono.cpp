6d73428031132d225b23b5f1cd8216f0d3b83883
Peter Vingelmann <petya@master.com>
2013-06-10 02:34:18 +0200

    adding test for comparing clock resolution

--- test/src/test_boost_chrono.cpp
+++ test/src/test_boost_chrono.cpp
@@ -23,13 +23,16 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
 #include <gtest/gtest.h>
 
 #include <boost/chrono.hpp>
-#include <boost/chrono/chrono_io.hpp>
-#include <boost/chrono/floor.hpp>
-#include <boost/chrono/round.hpp>
-#include <boost/chrono/ceil.hpp>
+#include <boost/thread.hpp>
+
+namespace bc = boost::chrono;
 
 TEST(TestBoostChrono, intervals)
 {
@@ -44,6 +47,38 @@ TEST(TestBoostChrono, intervals)
     auto s2 =  floor<frame_rate>(ms);
     s2 = round<frame_rate>(ms);
     s2 = ceil<frame_rate>(ms);
+}
+
+
+template <class Clock>
+double get_nanosec(typename Clock::duration delta)
+{
+    return (double)bc::duration_cast<bc::nanoseconds>(delta).count();
+}
+
+
+TEST(TestBoostChrono, resolution)
+{
+    using namespace boost::chrono;
+
+    system_clock::time_point d4 = system_clock::now();
+    system_clock::time_point d5 = system_clock::now();
+
+    std::cout << "system_clock delta (ns): " <<
+                 get_nanosec<system_clock>(d5 - d4) << std::endl;
+
+    steady_clock::time_point d6 = steady_clock::now();
+    steady_clock::time_point d7 = steady_clock::now();
+
+    std::cout << "steady_clock delta (ns): " <<
+                 get_nanosec<steady_clock>(d7 - d6) << std::endl;
+
+    high_resolution_clock::time_point d8 = high_resolution_clock::now();
+    high_resolution_clock::time_point d9 = high_resolution_clock::now();
+
+    std::cout << "high_resolution_clock delta (ns): " <<
+                 get_nanosec<high_resolution_clock>(d9 - d8) << std::endl;
+
 
 }
 
