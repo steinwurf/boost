f2f4ddb5094f077e911660a02680f23382ddddc2
Peter Vingelmann <petya@master.com>
2014-09-24 03:56:49 +0200

    Fix unused warnings in boost/spirit and boost/fusion

--- boost/spirit/home/karma/auxiliary/lazy.hpp
+++ boost/spirit/home/karma/auxiliary/lazy.hpp
@@ -61,6 +61,7 @@ namespace boost { namespace spirit { namespace karma
           , Context& context, Delimiter const& delim
           , Attribute const& attr, mpl::true_)
         {
+            (void) attr;
             // If DeducedAuto is false (semantic actions is present), the
             // component's attribute is unused.
             return g.generate(sink, context, delim, unused);
